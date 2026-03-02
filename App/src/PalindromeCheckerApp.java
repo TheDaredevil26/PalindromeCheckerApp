4d00e53f10c2f959478dafcde2adfd0c632ecb5d
Nishu-28 <nishu28n18v7d@gmail.com>
2026-03-02 10:05:49 +0530

    Use Case 2 added

--- App/src/PalindromeCheckerApp.java
+++ App/src/PalindromeCheckerApp.java
@@ -1,9 +1,30 @@
-public class PalindromeCheckerApp {
+import java.util.Scanner;
 
-        public static void main(String[] args){
-            System.out.println("Welcome to the Palindrome Checker Management System.");
-            System.out.println("Version : 1.0");
-            System.out.println("SYstem Initialised successfully.");
+public class PalindromeCheckerApp{
+    public static boolean isPalindrome(String s1) {
+        int i = 0;
+        int j = s1.length() - 1;
+
+        while (i < j) {
+            if (s1.charAt(i) != s1.charAt(j)) {
+                return false;
+            }
+            i++;
+            j--;
         }
+        return true;
     }
 
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter a string: ");
+        String s = input.nextLine();
+
+        if (isPalindrome(s)) {
+            System.out.println("\"" + s + "\" is a palindrome.");
+        } else {
+            System.out.println("\"" + s + "\" is not a palindrome.");
+        }
+        input.close();
+    }
+}
