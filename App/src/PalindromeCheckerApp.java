9bad9005d6537ccba10fa71f0fe11d9d02406493
TheDaredevil26 <sm5588@srmist.edu.in>
2026-03-02 11:10:47 +0530

    Use Case 10 uploaded

--- App/src/PalindromeCheckerApp.java
+++ App/src/PalindromeCheckerApp.java
@@ -1,27 +1,35 @@
+import java.util.Scanner;
+
 public class PalindromeCheckerApp {
+
+    /**
+     * Application entry point for UC10.
+     *
+     * @param args Command-Line arguments
+     */
     public static void main(String[] args) {
-        String input = "Level";
 
-        input = input.replaceAll("\\s+", "").toLowerCase();
+        Scanner scanner = new Scanner(System.in);
 
-        boolean result = check(input, 0, input.length() - 1);
+        System.out.print("Input : ");
+        String input = scanner.nextLine();
+        String normalized = input.replaceAll("[^a-zA-Z0-9]", "")
+                .toLowerCase();
 
-        if (result) {
-            System.out.println("The string is a Palindrome.");
-        } else {
-            System.out.println("The string is NOT a Palindrome.");
+        boolean isPalindrome = true;
+
+        for (int i = 0; i < normalized.length() / 2; i++) {
+            if (normalized.charAt(i) !=
+                    normalized.charAt(normalized.length() - 1 - i)) {
+
+                isPalindrome = false;
+                break;
+            }
         }
 
+        System.out.println("Input: " + input);
+        System.out.println("is Palindrome? " + isPalindrome);
+
+        scanner.close();
     }
-
-    private static boolean check(String s, int start, int end) {
-        if (start >= end) {
-            return true;
-        }
-        if (s.charAt(start) != s.charAt(end)) {
-            return false;
-        }
-        return check(s, start + 1, end - 1);
-    }
-
 }
